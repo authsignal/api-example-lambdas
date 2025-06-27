f9130498781e98e82fe48c4ca238067a7da39ba3
Chris Fisher <chris.i.fisher@gmail.com>
2025-06-27 14:50:34 +1200

    Use env client

--- api/sign-in.ts
+++ api/sign-in.ts
@@ -3,7 +3,6 @@ import { authsignal } from "../lib/authsignal";
 
 interface RequestBody {
   token: string;
-  clientId: string;
 }
 
 interface ResponseBody {
@@ -11,8 +10,10 @@ interface ResponseBody {
   refreshToken: string;
 }
 
+const clientId = process.env.AUTHSIGNAL_CLIENT!;
+
 export const handler = async (event: APIGatewayProxyEventV2): Promise<ResponseBody> => {
-  const { token, clientId } = JSON.parse(event.body!) as RequestBody;
+  const { token } = JSON.parse(event.body!) as RequestBody;
 
   const { accessToken, refreshToken } = await authsignal.createSession({
     token,
