9e57dd3caccc79349d82db9e72c1b977f089a417
Chris Fisher <chris.i.fisher@gmail.com>
2025-10-23 10:36:12 +1300

    Add push registration endpoint

--- api/init-push-registration.ts
+++ api/init-push-registration.ts
@@ -1,13 +1,19 @@
 import { APIGatewayProxyEventV2WithJWTAuthorizer } from "aws-lambda";
 import { authsignal } from "../lib/authsignal";
 
+// Generates an Authsignal token for registering for push verification via Client SDK
 export const handler = async (event: APIGatewayProxyEventV2WithJWTAuthorizer) => {
   const userId = event.requestContext.authorizer.jwt.claims.sub as string;
 
-  const user = await authsignal.getUser({ userId });
+  const { token } = await authsignal.track({
+    userId,
+    action: "registerPush",
+    attributes: {
+      scope: "add:authenticators",
+    },
+  });
 
   return {
-    userId,
-    ...user,
+    token,
   };
 };
